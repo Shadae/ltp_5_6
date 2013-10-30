fa321c1685e0dd25d846451685593ca82f01ce5a
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-10-29 22:00:15 -0700

    first check in for name greet

---
+++ namegreet.rb
@@ -0,0 +1,8 @@
+puts 'What is your first name?'
+fname = gets.chomp
+puts 'and what is your middle name?'
+mname = gets.chomp
+puts 'and finally what is your last name?'
+lname = gets.chomp
+#puts 'Hello ' + fname +' ' + mname +' '+ lname +' '+ ', Thank you' 
+puts "Hello #{fname} #{mname} #{lname} Thank you" 
