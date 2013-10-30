3b41243a7ddf636a0723cfee8cbc41aa188811f1
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-10-29 22:20:24 -0700

    first commit of favnum

---
+++ favnum.rb
@@ -0,0 +1,6 @@
+#favnumber
+puts 'What is your favorite number?'
+favnum = gets.chomp
+favnumint = favnum.to_i + 1
+puts "Oh you are correct #{favnum} is a great number, but #{favnumint} might be a bit better, since it is bigger."
+
